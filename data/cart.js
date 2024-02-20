cebea992282f3e6a1890ed584a6d4fcc33bc001e
panagopulos <tpanagopulos2@gmail.com>
2024-02-20 13:19:24 +0100

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
 //saves just the id which will then give us acces to the
 //rest of the information but we need to use products.js
-export let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if(!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 //Function which
 //Iterate through cart and checks if there is already
 //an MatchingItem if yes save adds just int +=1 
@@ -39,16 +47,20 @@ export function addToCart(productId) {
           quantity
       });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
-    const newCart = []
+    const newCart = [];
 
     cart.forEach((cartItem) => {
-        if (cartItem.productId !== producId) {
+        if (cartItem.productId !== productId) {
             newCart.push(cartItem);
         }
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
